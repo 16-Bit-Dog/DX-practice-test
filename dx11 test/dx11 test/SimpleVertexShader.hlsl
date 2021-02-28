566ec7e82707005e550c85ed0494b9d270a8e7f8
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-02-28 16:26:53 -0500

    Update SimpleVertexShader.hlsl

--- dx11 test/dx11 test/SimpleVertexShader.hlsl
+++ dx11 test/dx11 test/SimpleVertexShader.hlsl
@@ -20,13 +20,16 @@ struct AppData //vars in here are passed from application to vertex shader
     float3 position : POSITION; //vertex attribute var 
     float3 normal: NORMAL; // normal attribute var
     float2 tex : TEXCOORD; // texture coordinate
+    int texlink : TEXLINK;
 };
 
 struct VertexShaderOutput //vertex shader data to be passed to pixel shader
 {
     float4 position : SV_POSITION; //assign color var first to apply color before vertex shifting
     //float4 position : SV_POSITION; //assign vertex pos 
-    float2 tex : TEXCOORD; // I do not need normals to send to the SimplePixelShader for now, so... yeah
+    float3 normal: NORMAL;
+    float2 tex : TEXCOORD; // I do not need normals to send to the SimplePixelShader for now, so... 
+    uint texlink : TEXLINK;
 };
 
 VertexShaderOutput SimpleVertexShader(AppData IN) //entry point for vertex shader program - returns VertexShaderOutput struct for pixel shader, takes in appData struct
@@ -34,9 +37,13 @@ VertexShaderOutput SimpleVertexShader(AppData IN) //entry point for vertex shade
     VertexShaderOutput OUT;
 
     matrix mvp = mul(projectionMatrix, mul(viewMatrix, worldMatrix)); //-model to view to projection- is computed by doing projectionMatrix*(viewMatrix*worldMatrix)
-    OUT.position = mul(mvp, float4(IN.position, 1.0f)); //OUT.position stores vertex pos made into float4 to be passed to pixel shader
-    //OUT.color = float4(IN.color, 1.0f); //float4 is what we use for a pixel shader variable - so we declare a OUT.var for color in this sense (1.0 means we send the whole value, un modified - 0.9 would send faactor of 9 value
+    OUT.position = mul(mvp, float4(IN.position, 0.5f)); //OUT.position stores vertex pos made into float4 to be passed to pixel shader
+    
+//OUT.color = float4(IN.color, 1.0f); //float4 is what we use for a pixel shader variable - so we declare a OUT.var for color in this sense (1.0 means we send the whole value, un modified - 0.9 would send faactor of 9 value
     OUT.tex = IN.tex;
+    OUT.normal = mul(mvp, float4(IN.normal, 0.5f));
+    
+    OUT.texlink = IN.texlink;
 
     return OUT; //output will be going to pixel shader
 }

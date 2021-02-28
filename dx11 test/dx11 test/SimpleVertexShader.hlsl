adf9fd14c730772168086e92a33ba84fc9b5455e
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-02-28 10:19:16 -0500

    Update SimpleVertexShader.hlsl

--- dx11 test/dx11 test/SimpleVertexShader.hlsl
+++ dx11 test/dx11 test/SimpleVertexShader.hlsl
@@ -26,6 +26,7 @@ struct VertexShaderOutput //vertex shader data to be passed to pixel shader
 {
     float4 position : SV_POSITION; //assign color var first to apply color before vertex shifting
     //float4 position : SV_POSITION; //assign vertex pos 
+    float3 normal: NORMAL;
     float2 tex : TEXCOORD; // I do not need normals to send to the SimplePixelShader for now, so... yeah
 };
 
@@ -35,8 +36,10 @@ VertexShaderOutput SimpleVertexShader(AppData IN) //entry point for vertex shade
 
     matrix mvp = mul(projectionMatrix, mul(viewMatrix, worldMatrix)); //-model to view to projection- is computed by doing projectionMatrix*(viewMatrix*worldMatrix)
     OUT.position = mul(mvp, float4(IN.position, 1.0f)); //OUT.position stores vertex pos made into float4 to be passed to pixel shader
-    //OUT.color = float4(IN.color, 1.0f); //float4 is what we use for a pixel shader variable - so we declare a OUT.var for color in this sense (1.0 means we send the whole value, un modified - 0.9 would send faactor of 9 value
+    
+//OUT.color = float4(IN.color, 1.0f); //float4 is what we use for a pixel shader variable - so we declare a OUT.var for color in this sense (1.0 means we send the whole value, un modified - 0.9 would send faactor of 9 value
     OUT.tex = IN.tex;
-
+    OUT.normal = IN.normal;
+    
     return OUT; //output will be going to pixel shader
 }

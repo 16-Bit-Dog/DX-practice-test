3e894a07fc7c41cc29de297e1752de801f888a29
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-03-05 23:20:09 -0500

    acompanys the previous PR
    was lazy to make a branch since I would anyways rather separate these PR's - not like I *need* to squash PR's

--- dx11 test/dx11 test/SimpleComputeShader.hlsl
+++ dx11 test/dx11 test/SimpleComputeShader.hlsl
@@ -1,9 +1,15 @@
-////////NOT WORKING PLACE HOLDER
 
+RWTexture2D<unorm float4> OutputBuffer : register(u0);
 
-//--------------------------------------------------------------------------------------
-[numthreads(1, 1, 1)]
-void SimpleComputeShader(uint3 DTid : SV_DispatchThreadID)
+[numthreads(8, 8, 1)]
+void SimpleComputeShader(uint3 groupId : SV_GroupID,
+    uint3 groupThreadId : SV_GroupThreadID,
+    uint3 dispatchThreadId : SV_DispatchThreadID,
+    uint groupIndex : SV_GroupIndex)
 {
+    unorm float4 w = (0.1, 0.1, 0.1, 0.1);
+    //Set the structured buffer to weird stuff
+    OutputBuffer[dispatchThreadId.xy] = OutputBuffer.Load(int2(dispatchThreadId.x, dispatchThreadId.y)) + w; // load - loads a pixel
+
 
 }

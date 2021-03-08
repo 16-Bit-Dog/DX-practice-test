9cad2f2378c91691fc171b7ec38a420e0f3bf1ce
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-03-08 15:55:30 -0500

    time based compute shader computation

--- dx11 test/dx11 test/SimpleComputeShader.hlsl
+++ dx11 test/dx11 test/SimpleComputeShader.hlsl
@@ -1,8 +1,20 @@
-
+//texture2D InputTex : register(t0);
 RWTexture2D<unorm float4> OutputBuffer : register(u0); //0 is always render targets
 
-[numthreads(32, 32, 1)]
+cbuffer PerTime : register(b3) //const buffer - for stuff needed once per program life time - group shared for many thread concurrent acsess
+{
+    matrix ConstantUnsortedTypes; //random DATA
+    //[0][0] is time
+}
+
+[numthreads(32, 32, 1)] //32 * 32 means 1024 pixels --> add z if larger is used - but for now this is what I use - will later
 void SimpleComputeShader(uint3 DispatchThreadID : SV_DispatchThreadID)
 {
-    OutputBuffer[DispatchThreadID.xy] = float4(0.f, 1.f, 0.f, 0.f);
+
+    float r = OutputBuffer[DispatchThreadID.xy].r + float(sin(ConstantUnsortedTypes[0][0]) / 100);
+    float g = OutputBuffer[DispatchThreadID.xy].g + float(sin(ConstantUnsortedTypes[0][0]) / 100);
+    float b = OutputBuffer[DispatchThreadID.xy].b;
+    float w = OutputBuffer[DispatchThreadID.xy].w;
+
+    OutputBuffer[DispatchThreadID.xy] = float4(r, g, b, w);
 }

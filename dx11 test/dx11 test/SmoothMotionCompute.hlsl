731b892b3ae03982cfc4c2c08ecd3d40a59ffe38
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-03-14 10:51:40 -0400

    RWByteAddressBuffer addition

--- dx11 test/dx11 test/SmoothMotionCompute.hlsl
+++ dx11 test/dx11 test/SmoothMotionCompute.hlsl
@@ -1,17 +1,18 @@
-// WORK IN PROGRESS
-
 //texture2D InputTex : register(t0);
 
-struct vertexLayout {
+struct AppData //used as demonstration of data
+{
+    float3 position : POSITION; //vertex attribute var    //12 bytes
+    //4
+    //4
+    //4
 
-    float x;
-    float y;
-    float z;
-    float w; 
-
+    float3 normal: NORMAL; // normal attribute var        //12 bytes
+    float2 tex : TEXCOORD; // texture coordinate          //8 bytes
+    int texlink : TEXLINK; //4 bytes
 };
 
-RWBuffer<vertexLayout> OutputBuffer : register(u0); //1 is the vertex buffer 
+RWByteAddressBuffer OutputBuffer : register(u0); //1 is the vertex buffer 
 
 cbuffer PerTime : register(b3) //const buffer - for stuff needed once per program life time - group shared for many thread concurrent acsess
 {
@@ -19,20 +20,41 @@ cbuffer PerTime : register(b3) //const buffer - for stuff needed once per progra
     //[0][0] is time
 }
 
-[numthreads(32, 32,1)] //32 * 32 means 1024 pixels --> add z if larger is used - but for now this is what I use - will later
+[numthreads(32, 32, 1)] //32 * 32 means 1024 pixels --> add z if larger is used - but for now this is what I use - will later
 void SmoothMotionCompute(uint3 DispatchThreadID : SV_DispatchThreadID)
 {
     uint index = DispatchThreadID.x * DispatchThreadID.y;
 
-    vertexLayout vertexC;
-
-    vertexC.x = 0.f;
-    vertexC.y = 0.f;
-    vertexC.z = 0.f;
-    vertexC.w = 0.f;
+//    vertexC.position = float3(0.f, 0.f, 0.f);
+//    vertexC.normal = OutputBuffer[index].normal;
+//    vertexC.tex = OutputBuffer[index].tex;
+//    vertexC.texlink = OutputBuffer[index].texlink;
 
 
-    OutputBuffer[index] = vertexC;
+
+    uint i = (index * 36) - 36;
+
+        float3 in_position = asfloat(OutputBuffer.Load3(i));
+        float3 in_color = asfloat(OutputBuffer.Load3(i + 12));
+        float2 in_tex = asfloat(OutputBuffer.Load2(i + 24));
+        float in_texlink = asfloat(OutputBuffer.Load(i + 32));
+        in_position[0] = 0.f;
+        in_position[1] = 0.f;
+        in_position[2] = 0.f;
+
+        in_color[0] = 0.f;
+        in_color[1] = 0.f;
+        in_color[2] = 0.f;
+
+        in_tex[0] = 0.f;
+        in_tex[1] = 0.f;
+
+        in_texlink = 0.f;
+
+        OutputBuffer.Store3(i, asuint(in_position));
+        OutputBuffer.Store3(i+12, asuint(in_color));
+        OutputBuffer.Store2(i+24, asuint(in_tex));
+        OutputBuffer.Store(i+32, asuint(in_texlink));
 
     
     /*

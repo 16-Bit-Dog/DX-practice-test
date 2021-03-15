4e7746aad252f3f28197bc99ae9a4a1f6815782d
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-03-14 21:07:04 -0400

    more fun

--- dx11 test/dx11 test/SmoothMotionCompute.hlsl
+++ dx11 test/dx11 test/SmoothMotionCompute.hlsl
@@ -3,9 +3,9 @@
 struct AppData //used as demonstration of data
 {
     float3 position : POSITION; //vertex attribute var    //12 bytes
-    //4
-    //4
-    //4
+    //4 - x
+    //4 - y
+    //4 - z
 
     float3 normal: NORMAL; // normal attribute var        //12 bytes
     float2 tex : TEXCOORD; // texture coordinate          //8 bytes
@@ -20,17 +20,21 @@ cbuffer PerTime : register(b3) //const buffer - for stuff needed once per progra
     //[0][0] is time
 }
 
-[numthreads(32, 32, 1)] //32 * 32 means 1024 pixels --> add z if larger is used - but for now this is what I use - will later
-void SmoothMotionCompute(uint3 DispatchThreadID : SV_DispatchThreadID)
+[numthreads(1024, 1, 1)] //threads to launch inside compute shader
+void SmoothMotionCompute(uint ids : SV_DispatchThreadID)
 {
-    uint index = DispatchThreadID.x * DispatchThreadID.y;
+    uint id = ids.x;
 
-    if (index < 100) {
-        OutputBuffer[index].position = float3(sin(ConstantUnsortedTypes[0][0]), sin(ConstantUnsortedTypes[0][0]), sin(ConstantUnsortedTypes[0][0]));
-    }
-    OutputBuffer[index].normal = OutputBuffer[index].normal;
-    OutputBuffer[index].tex = OutputBuffer[index].tex;
-    OutputBuffer[index].texlink = OutputBuffer[index].texlink;
+    float3 posModData = OutputBuffer[id].position;
+
+    float posModx = ((sin(ConstantUnsortedTypes[0][0]) / 100 + posModData[0]));
+    float posMody = ((sin(ConstantUnsortedTypes[0][0]) / 100 + posModData[1]));
+    float posModz = ((sin(ConstantUnsortedTypes[0][0]) / 100 + posModData[2]));
+
+    OutputBuffer[id].position = float3(posModx, posMody, posModz);
+    OutputBuffer[id].normal = OutputBuffer[id].normal;
+    OutputBuffer[id].tex = OutputBuffer[id].tex;
+    OutputBuffer[id].texlink = OutputBuffer[id].texlink;
 
 //    vertexC.position = float3(0.f, 0.f, 0.f);
 //    vertexC.normal = OutputBuffer[index].normal;

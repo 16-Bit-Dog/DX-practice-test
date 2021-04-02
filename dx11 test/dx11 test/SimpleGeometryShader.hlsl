03f9103a05b21ac546a2e72b0e0b626426287494
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-04-02 14:52:46 -0400

    fixed my Geo shader semantics and setup
    I forgot about pixel shader interoperability and did not have pixel shader input var's

--- dx11 test/dx11 test/SimpleGeometryShader.hlsl
+++ dx11 test/dx11 test/SimpleGeometryShader.hlsl
@@ -27,22 +27,46 @@ cbuffer PerLight1 : register(b4) //const buffer - for stuff needed once per prog
 
 struct GSOutput
 {
-    float4 pos : POSITION; //vertex attribute var 
+    float4 pos : SV_POSITION; //assign color var first to apply color before vertex shifting
+    //float4 position : SV_POSITION; //assign vertex pos 
+    float3 norm: NORMAL;
+    float2 tex : TEXCOORD0; // I do not need normals to send to the SimplePixelShader for now, so... 
+    uint texl : TEXLINK;
+    float4 posw : TEXCOORD1;
 };
 
 [maxvertexcount(6)] // this geo shader takes 1 vertex from current vertex buffer and makes 6 vertex triangle stream out of them - makes a fuzzy multiplication of vertex effect
-//by design all made output stream triangles follow the normal, color, and which texture to use symantic from the original input
+//each tri stream has its own values based on the vertex submitted
 void SimpleGeometryShader(point GSOutput input[1], inout TriangleStream<GSOutput> OutputStream)
 {
     GSOutput gsout;
 
     gsout.pos = float4(input[0].pos.x + 0.5, input[0].pos.y + 0.5, input[0].pos.z, input[0].pos.w);
+    gsout.posw = float4(input[0].pos.x + 0.5, input[0].pos.y + 0.5, input[0].pos.z, input[0].pos.w);
+    gsout.norm = input[0].norm;
+    gsout.tex = input[0].tex;
+    gsout.texl = input[0].texl;
     OutputStream.Append(gsout);
+
     gsout.pos = float4(input[0].pos.x - 0.5, input[0].pos.y + 0.5, input[0].pos.z, input[0].pos.w);
+    gsout.posw = float4(input[0].pos.x - 0.5, input[0].pos.y + 0.5, input[0].pos.z, input[0].pos.w);
+    gsout.norm = input[0].norm;
+    gsout.tex = input[0].tex;
+    gsout.texl = input[0].texl;
     OutputStream.Append(gsout);
+
     gsout.pos = float4(input[0].pos.x - 0.5, input[0].pos.y - 0.5, input[0].pos.z, input[0].pos.w);
+    gsout.posw = float4(input[0].pos.x - 0.5, input[0].pos.y - 0.5, input[0].pos.z, input[0].pos.w);
+    gsout.norm = input[0].norm;
+    gsout.tex = input[0].tex;
+    gsout.texl = input[0].texl;
     OutputStream.Append(gsout);
+    
     gsout.pos = float4(input[0].pos.x + 0.5, input[0].pos.y - 0.5, input[0].pos.z, input[0].pos.w);
+    gsout.posw = float4(input[0].pos.x - 0.5, input[0].pos.y - 0.5, input[0].pos.z, input[0].pos.w);
+    gsout.norm = input[0].norm;
+    gsout.tex = input[0].tex;
+    gsout.texl = input[0].texl;
     OutputStream.Append(gsout);
 
     

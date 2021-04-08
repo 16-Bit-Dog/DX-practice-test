4903bc1515d85f9c864c279fdafdb7dbd37ea0ed
16-Bit-Dog <67922228+16-Bit-Dog@users.noreply.github.com>
2021-04-08 13:19:26 -0400

    Update SmoothMotionCompute.hlsl

--- dx11 test/dx11 test/SmoothMotionCompute.hlsl
+++ dx11 test/dx11 test/SmoothMotionCompute.hlsl
@@ -26,15 +26,9 @@ void SmoothMotionCompute(uint ids : SV_DispatchThreadID)
 
     float3 posModData = OutputBuffer[id].position;
 
-    /*
-    * 
-    *move object - static all x, y, then z
-    * 
-    float posModx = ((sin(ConstantUnsortedTypes[0][0]) / 100 + posModData[0]));
-    float posMody = ((sin(ConstantUnsortedTypes[0][0]) / 100 + posModData[1]));
-    float posModz = ((sin(ConstantUnsortedTypes[0][0]) / 100 + posModData[2]));
-    */
-    float time = ConstantUnsortedTypes[0][0];
+    float time = ConstantUnsortedTypes[0];
+    float RatioRMS = ConstantUnsortedTypes[1];
+    float RealSound = ConstantUnsortedTypes[2];
 
     float posModx = (posModData[0]);
     float posMody = (posModData[1]);
@@ -42,127 +36,33 @@ void SmoothMotionCompute(uint ids : SV_DispatchThreadID)
     
     uint rem = id % 3;
 
-    //posModx += (sin(2 * time) * (sin(time) / log(time))) / (100 * (rem + 1));
-    //posMody += (sin(2 * time) * (sin(time) / log(time))) / (100 * (rem + 1));
-    //posModz += (sin(2 * time) * (sin(time) / log(time))) / (100 * (rem + 1));
+    if (id*rem< 10*RatioRMS*1024) {
 
-    posModx += sin(2 * time) / (10 * (rem + 1));
-    posMody += sin(2 * time) / (10 * (rem + 1));
-    posModz += sin(2 * time) / (10 * (rem + 1));
+        posModx -= RealSound/100;
+        posMody -= RealSound/100;
+        posModz -= RealSound/100;
 
-    //time to move every other vertex to simulate water - AN IMPORTANT THING TO KNOW IS THAT THE MODEL IS EVEN, so reminder is fine
-    //if (id % 10 == 0) {
-    //    //posModx += sin();
-    //    posModx += (sin(2 * time) * (sin(time) / log(time))) / 100;
-    //    posMody += (sin(2 * time) * (sin(time) / log(time))) / 100;
-    //    posMody += (sin(2 * time) * (sin(time) / log(time))) / 100;
+    }
+    else {
+        posModx -= 0.01;
+        posMody -= 0.01;
+        posModz -= 0.01;
 
-    //}
-    //else if (id % 10 == 1) {
-    //    //posModx += sin();
-    //    posMody += sin(ConstantUnsortedTypes[0][0]) / 1900;
-    //    posModx += sin(ConstantUnsortedTypes[0][0]) / 1900;
-    //    posModz += cos(ConstantUnsortedTypes[0][0]) / 2100;
-    //}
 
-    //else if (id % 10 == 2) {
-    //    //posModx += sin();
-    //    posMody += sin(ConstantUnsortedTypes[0][0]) / 2100;
-    //    posModx += sin(ConstantUnsortedTypes[0][0]) / 2100;
-    //    posModz += cos(ConstantUnsortedTypes[0][0]) / 2200;
-    //}
+    }
+    if (posModx < -2 || posMody < -2 || posModz < -2) {
+        posModx += 2;
+        posMody += 2;
+        posModz += 2;
+    }
 
-    //else if (id % 10 == 3) {
-    //    //posModx += sin();
-    //    posMody += sin(ConstantUnsortedTypes[0][0]) / 2500;
-    //    posModx += sin(ConstantUnsortedTypes[0][0]) / 2500;
-    //    posModz += cos(ConstantUnsortedTypes[0][0]) / 3000;
-    //}
-
-    //else if (id % 10 == 4) {
-    //    //posModx += sin();
-    //    posMody += sin(ConstantUnsortedTypes[0][0]) / 3000;
-    //    posModx += sin(ConstantUnsortedTypes[0][0]) / 3000;
-    //    posModz += cos(ConstantUnsortedTypes[0][0]) / 3500;
-    //}
-
-    //else if (id % 10 == 5) {
-    //    //posModx += sin();
-    //    posMody += sin(ConstantUnsortedTypes[0][0]) / 3500;
-    //    posModx += sin(ConstantUnsortedTypes[0][0]) / 3500;
-    //    posModz += cos(ConstantUnsortedTypes[0][0]) / 4000;
-    //}
-
-    //else if (id % 10 == 6) {
-    //    //posModx += sin();
-    //    posMody += sin(ConstantUnsortedTypes[0][0]) / 4000;
-    //    posModx += sin(ConstantUnsortedTypes[0][0]) / 4000;
-    //    posModz += cos(ConstantUnsortedTypes[0][0]) / 5300;
-    //}
-
-    //else if (id % 10 == 7) {
-    //    //posModx += sin();
-    //    posMody += sin(ConstantUnsortedTypes[0][0]) / 4500;
-    //    posModx += sin(ConstantUnsortedTypes[0][0]) / 4800;
-    //    posModz += cos(ConstantUnsortedTypes[0][0]) / 5500;
-    //}
-
-    //else if (id % 10 == 8) {
-    //    //posModx += sin();
-    //    posMody += sin(ConstantUnsortedTypes[0][0]) / 5700;
-    //    posModx += sin(ConstantUnsortedTypes[0][0]) / 5700;
-    //    posModz += cos(ConstantUnsortedTypes[0][0]) / 5800;
-    //}
-
+    //posModx += sin(2 * time) / (10 * (rem + 1));
+    //posMody += sin(2 * time) / (10 * (rem + 1));
+    //posModz += sin(2 * time) / (10 * (rem + 1));
 
     OutputBuffer[id].position = float3(posModx, posMody, posModz);
     OutputBuffer[id].normal = OutputBuffer[id].normal;
     OutputBuffer[id].tex = OutputBuffer[id].tex;
     OutputBuffer[id].texlink = OutputBuffer[id].texlink;
-
-//    vertexC.position = float3(0.f, 0.f, 0.f);
-//    vertexC.normal = OutputBuffer[index].normal;
-//    vertexC.tex = OutputBuffer[index].tex;
-//    vertexC.texlink = OutputBuffer[index].texlink;
-
-
-
-  /*  uint i = (index * 36) - 36;
-
-        float3 in_position = asfloat(OutputBuffer.Load3(i));
-        float3 in_color = asfloat(OutputBuffer.Load3(i + 12));
-        float2 in_tex = asfloat(OutputBuffer.Load2(i + 24));
-        float in_texlink = asfloat(OutputBuffer.Load(i + 32));
-        in_position[0] = 0.f;
-        in_position[1] = 0.f;
-        in_position[2] = 0.f;
-
-        in_color[0] = 0.f;
-        in_color[1] = 0.f;
-        in_color[2] = 0.f;
-
-        in_tex[0] = 0.f;
-        in_tex[1] = 0.f;
-
-        in_texlink = 0.f;
-
-        OutputBuffer.Store3(i, asuint(in_position));
-        OutputBuffer.Store3(i+12, asuint(in_color));
-        OutputBuffer.Store2(i+24, asuint(in_tex));
-        OutputBuffer.Store(i+32, asuint(in_texlink));
-
-    
-    /*
-    tmp[0] = 0.0f;
-    tmp[1] = 0.0f;
-    tmp[2] = 0.0f;
-    tmp[3] = 0.0f;
-    */
-    
-    //float r = OutputBuffer[DispatchThreadID.xy].r + float(sin(ConstantUnsortedTypes[0][0]) / 100);
-    //float g = OutputBuffer[DispatchThreadID.xy].g + float(sin(ConstantUnsortedTypes[0][0]) / 100);
-    //float b = OutputBuffer[DispatchThreadID.xy].b;
-    //float w = OutputBuffer[DispatchThreadID.xy].w;
-
 
 }
